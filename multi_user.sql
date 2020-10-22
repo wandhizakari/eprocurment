216099e2edc7668cfe2e0e067c25d782a6a17fb5
wandhizakari <wandhi.zakaria@e.sicepat.com>
2020-10-22 20:19:17 +0700

    Menambahkan fungsi di bagian Pengadaan

--- multi_user.sql
+++ multi_user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 21, 2020 at 06:28 PM
+-- Generation Time: Oct 22, 2020 at 03:18 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.31
 
@@ -20,6 +20,34 @@ SET time_zone = "+00:00";
 --
 -- Database: `multi_user`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengadaan`
+--
+
+CREATE TABLE `pengadaan` (
+  `id_pengadaan` int(11) NOT NULL,
+  `nama_barang` varchar(300) NOT NULL,
+  `jumlah` int(100) NOT NULL,
+  `spesifikasi` text NOT NULL,
+  `tanggal_pengadaan` varchar(30) NOT NULL,
+  `admin_pengadaan` int(100) NOT NULL,
+  `status` varchar(30) NOT NULL,
+  `id_supplier` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengadaan`
+--
+
+INSERT INTO `pengadaan` (`id_pengadaan`, `nama_barang`, `jumlah`, `spesifikasi`, `tanggal_pengadaan`, `admin_pengadaan`, `status`, `id_supplier`) VALUES
+(9, 'beras', 3, 'dipasok sebelum tanggal 20 oktober 2015', '12-10-2014', 1, 'approved', 0),
+(10, 'tahu', 3, 'dipasok sebelum tanggal 20 oktober 2015', '12-10-2014', 1, 'approved', 0),
+(11, 'dsa', 3, 'dipasok sebelum tanggal 20 oktober 2015', '12-10-2014', 1, 'approved', 0),
+(12, 'beras', 3, 'dipasok sebelum tanggal 20 oktober 2015', '12-10-2014', 1, 'approved', 0),
+(13, 'beras', 4, 'dipasok sebelum tanggal 21 oktober 2215', '12-10-2014', 1, 'approved', 0);
 
 -- --------------------------------------------------------
 
@@ -42,11 +70,18 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `nama`, `username`, `password`, `level`) VALUES
 (1, 'wandhi', 'wandhi', 'wandhi123', 'procurment'),
 (2, 'dwi', 'dwi', 'dwi123', 'supplier'),
-(3, 'kobid', 'kobid', 'kobid123', 'purchasing');
+(3, 'kobid', 'kobid', 'kobid123', 'purchasing'),
+(4, 'manajer', 'manajer', 'manajer123', 'manajer');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `pengadaan`
+--
+ALTER TABLE `pengadaan`
+  ADD PRIMARY KEY (`id_pengadaan`);
 
 --
 -- Indexes for table `user`
@@ -59,10 +94,16 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `pengadaan`
+--
+ALTER TABLE `pengadaan`
+  MODIFY `id_pengadaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
